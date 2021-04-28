0af64652e4c29b01fcf6ae17fd487f8d49c6c82e
HimachalThakur <himachalthakur1@gmail.com>
2021-04-28 20:45:17 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  student = {
+    id: null,
+    fname: null,
+    lname: null,
+    email: null,
+    geneder: null
+  };
+
+  jsonObj = [
+    {
+      id: 1,
+      first_name: "Jeanette",
+      last_name: "Penddreth",
+      email: "jpenddreth0@census.gov",
+      gender: "Female"
+    },
+    {
+      id: 2,
+      first_name: "Giavani",
+      last_name: "Frediani",
+      email: "gfrediani1@senate.gov",
+      gender: "Male"
+    },
+    {
+      id: 3,
+      first_name: "Noell",
+      last_name: "Bea",
+      email: "nbea2@imageshack.us",
+      gender: "Female"
+    },
+    {
+      id: 4,
+      first_name: "Willard",
+      last_name: "Valek",
+      email: "wvalek3@vk.com",
+      gender: "Male"
+    }
+  ];
 }
